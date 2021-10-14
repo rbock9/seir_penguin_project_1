67bda1f21688e99dc7eb1dd572c5584432474647
Rob Bock <rbock9@gmail.com>
2021-10-14 13:03:07 -0400

    Sound effects, music, and win conditions added. Basically done, just a few minor tweaks left.

--- app.js
+++ app.js
@@ -5,13 +5,10 @@
 const state = {
     player1: 0,
     player2: 0,
-    currentQuestion: {},
     which: true
 }
 
 let questions = []
-
-
 
 ///////////////////////
 // MAIN DOM ELEMENTS //
@@ -24,6 +21,26 @@ const $c = $("#c")
 const $d = $("#d")
 const $p1score = $("#player1 h4")
 const $p2score = $("#player2 h4")
+
+///////////////////////
+// SOUNDS AND MUSIC  //
+///////////////////////
+
+// NOTE: decided not to use most of these since they get annoying very quickly
+
+// const bgMusic = new Audio('./sounds/wario_music.mp3')
+// bgMusic.play();
+// bgMusic.volume = 0.5;
+// bgMusic.loop = true;
+
+const missSound = new Audio('./sounds/wario_missed.wav')
+// missSound.play();
+
+const hitSound = new Audio('./sounds/wario_laugh.wav')
+// hitSound.play();
+
+const gonnaWinSound = new Audio('./sounds/wario_gonna_win.wav')
+// gonnaWinSound.play();
 
 ///////////////////////
 // FUNCTIONS //
@@ -62,10 +79,18 @@ const setBoard = (q) => {
     $p1score.text(state.player1)
     $p2score.text(state.player2)
 
-    $("li").off
+    $("li").off()
     $("li").on("click", (event) => {
         chooseAnswer(event, randomQuestion)
     })
+
+    if(state.player1 == 5) {
+        alert("Player 1 Wins! Pretty easy, huh? Refresh the page to play again, or don't (if you want to keep playing my awesome game). Either way, I'm gettin' outta this dump. Have a rotten day! Ehehehehe!")
+        hitSound.play();
+    } else if (state.player2 == 5) {
+        alert("Player 2 Wins! Pretty easy, huh? Refresh the page to play again, or don't (if you want to keep playing my awesome game). Either way, I'm gettin' outta this dump. Have a rotten day! Ehehehehe!")
+        hitSound.play();
+    }
 
 }
 
@@ -73,7 +98,6 @@ const setBoard = (q) => {
 ///////////////////////
 // MAIN APP LOGIC //
 ///////////////////////
-
 
 const URL = "https://cdn.contentful.com/spaces/wzvjort7ry1s/environments/master/entries?access_token=-eiQG5lPuIZQKafnkI9odfjVMRJ_pvXnhLZ04XvKpC8&content_type=triviaq"
 
@@ -87,31 +111,11 @@ $.ajax(URL)
 })
 
 
-///////////////////////
-// SOUNDS AND MUSIC  //
-///////////////////////
+alert("WAAAHAHAHA! I'm-a Wario, and welcome to my quiz game! Find a loser - err, I mean, friend - to compete with. The first player to earn 5 points wins the game. Here we go!")
 
-var bgMusic = new Audio('./sounds/wario_music.mp3')
-// bgMusic.play();
-// bgMusic.volume = 0.5;
-// bgMusic.loop = true;
+gonnaWinSound.play();
 
-var missSound = new Audio('./sounds/wario_missed.wav')
-// missSound.play();
-
-var hitSound = new Audio('./sounds/wario_laugh.wav')
-// hitSound.play();
-
-var gonnaWinSound = new Audio('./sounds/wario_gonna_win.wav')
-// gonnaWinSound.play();
+//////////////
 
 
-
-/////////////
-
-
-// Sample URL: https://cdn.contentful.com/spaces/{space_id}/environments/{environment_id}/entries?access_token={access_token}&content_type={content_type}
-
-// URL with space id (wzvjort7ry1s) and access token, but missing content type https://cdn.contentful.com/spaces/wzvjort7ry1s/environments/master/entries?access_token=-eiQG5lPuIZQKafnkI9odfjVMRJ_pvXnhLZ04XvKpC8 
-
-// Final URL with everything needed:  https://cdn.contentful.com/spaces/wzvjort7ry1s/environments/master/entries?access_token=-eiQG5lPuIZQKafnkI9odfjVMRJ_pvXnhLZ04XvKpC8&content_type=triviaq
+// URL for AJAX request:  https://cdn.contentful.com/spaces/wzvjort7ry1s/environments/master/entries?access_token=-eiQG5lPuIZQKafnkI9odfjVMRJ_pvXnhLZ04XvKpC8&content_type=triviaq
